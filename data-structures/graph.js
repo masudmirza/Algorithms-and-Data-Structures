8d3d3bf9f8bb792deed9c7728df398161fd1aee7
mesud <mesudmirzezade717@gmail.com>
2023-07-22 21:24:06 +0400

    graph

---
+++ data-structures/graph.js
@@ -0,0 +1,140 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacencyVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacencyVertex);
+    }
+
+    delete this.adjacencyList[vertex];
+  }
+
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+
+      adjacencyList[vertex].forEach(neighbour => {
+        if (!visited[neighbour]) dfs(neighbour);
+      });
+    })(start);
+
+    return result;
+  }
+
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while(stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbour => {
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          stack.push(neighbour);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  breadthFirst(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    visited[start] = true;
+    let currentVertex;
+
+    while(queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbour => {
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          queue.push(neighbour);
+        }
+      });
+      // this.adjacencyList[currentVertex].slice().reverse().forEach(neighbour => {
+      //   if (!visited[neighbour]) {
+      //     visited[neighbour] = true;
+      //     queue.push(neighbour);
+      //   }
+      // });
+    }
+
+    return result;
+  }
+}
+
+let g = new Graph();
+
+// g.addVertex('Dallas');
+// g.addVertex('Tokyo');
+// g.addVertex('Aspen');
+// g.addVertex('Los Angeles');
+// g.addVertex('Hong Kong');
+// console.log(g);
+// g.addEdge('Dallas', 'Tokyo');
+// g.addEdge('Dallas', 'Aspen');
+// g.addEdge('Hong Kong', 'Tokyo');
+// g.addEdge('Hong Kong', 'Dallas');
+// g.addEdge('Los Angeles', 'Hong Kong');
+// g.addEdge('Los Angeles', 'Aspen');
+// console.log(g);
+
+
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A','B');
+g.addEdge('A','C');
+g.addEdge('B','D');
+g.addEdge('C','E');
+g.addEdge('D','E');
+g.addEdge('D','F');
+g.addEdge('E','F');
+
+//            A
+//          /   \
+//         B     C
+//         |     |
+//         D --- E
+//          \   /
+//            F
+
+console.log(g.depthFirstRecursive('A'));
+console.log(g.depthFirstIterative('A'));
+console.log(g.breadthFirst('A'));
